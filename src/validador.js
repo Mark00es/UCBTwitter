6a7b8755b21ab1c75181d67908ed4f8b9b33d1c2
Marco <marco.escobar@ucb.edu.bo>
2022-03-10 12:10:41 -0400

    Limitar50

--- src/validador.js
+++ src/validador.js
@@ -1,10 +1,11 @@
-function validador(a) {
-    var resp = a
-    if(a != "")
-        resp = a
+function validador(titulo) {
+    var resp = titulo  
+    if(titulo.length < 50 && titulo != "")
+        resp = titulo
     else
-        alert("No es posible publicar un post sin título")
-    return a;
+        alert("No es posible publicar este post")
+    
+        return titulo;
   }
   
   export default validador;
